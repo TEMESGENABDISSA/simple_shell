8e2f871ec37bd56aa45f692c045db1cb3a201856
Temesgen Abdissa <temesgenabdissa2@gmail.com>
2023-05-24 10:49:27 -0700

    c-shell

--- string.c
+++ string.c
@@ -1,10 +1,10 @@
 #include "shell.h"
 
 /**
- * _strlen - returns the length of a string
- * @s: the string whose length to check
- *
- * Return: integer length of string
+ * _strlen - displays  length of str
+ * @s:  str  length to check
+ * author : temesgen abdissa and amanuel dessalegn
+ * Return:  returns int length for  the  str
  */
 int _strlen(char *s)
 {
@@ -19,11 +19,11 @@ int _strlen(char *s)
 }
 
 /**
- * _strcmp - performs lexicogarphic comparison of two strangs.
- * @s1: the first strang
- * @s2: the second strang
- *
- * Return: negative if s1 < s2, positive if s1 > s2, zero if s1 == s2
+ * _strcmp - perform  lexicogarphic compare of 2 str.
+ * @s1: first strings
+ * @s2: second strings
+ * author: temesgen abdissa and amanuel dessalegn
+ * Return: returns -ve if s1 < s2 and +ve if s1 > s2 else  0 if s1 == s2
  */
 int _strcmp(char *s1, char *s2)
 {
@@ -41,11 +41,11 @@ int _strcmp(char *s1, char *s2)
 }
 
 /**
- * starts_with - checks if needle starts with haystack
- * @haystack: string to search
- * @needle: the substring to find
- *
- * Return: address of next char of haystack or NULL
+ * starts_with - check  needle if start withhaystack
+ * @haystack: str to search
+ * @needle:  substring to find
+ * author: temesgen abdissa and amanuel dessalegn
+ * Return: address of the next char haystack or NULL
  */
 char *starts_with(const char *haystack, const char *needle)
 {
@@ -56,11 +56,11 @@ char *starts_with(const char *haystack, const char *needle)
 }
 
 /**
- * _strcat - concatenates two strings
- * @dest: the destination buffer
- * @src: the source buffer
- *
- * Return: pointer to destination buffer
+ * _strcat - concatenated 2 strs
+ * @dest: destinations  buffer
+ * @src:  src buffer
+ * author: temesgen abdissa and amanuel dessalegn
+ * Return: point destinations buffer
  */
 char *_strcat(char *dest, char *src)
 {
